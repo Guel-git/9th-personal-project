5b91006cc5ea0352b5efee288e607702a058066d
Guel-git <eccyuna@gmail.com>
2021-10-07 17:01:27 +0900

    프론트 완성

--- amble/account/forms.py
+++ amble/account/forms.py
@@ -5,5 +5,5 @@ from django import forms
 class RegisterForm(UserCreationForm):
     class Meta:
         model = CustomUser
-        fields = ['username', 'password1', 'password2', 'nickname']
-        widgets = {'username': forms.TextInput(attrs={'class': 'form-control', 'placeholder': '아이디'}), 'password1': forms.PasswordInput(attrs={'class': 'form-control','placeholder': '비밀번호'}), 'password2': forms.PasswordInput(attrs={'class': 'form-control', 'placeholder': '비밀번호 확인'}), 'nickname': forms.TextInput(attrs={'class': 'form-control', 'placeholder': '닉네임'})}
+        fields = ['username', 'password1', 'password2']
+        widgets = {'username': forms.TextInput(attrs={'class': 'form-control', 'placeholder': '아이디'}), 'password1': forms.PasswordInput(attrs={'class': 'form-control','placeholder': '비밀번호'}), 'password2': forms.PasswordInput(attrs={'class': 'form-control', 'placeholder': '비밀번호 확인'})}
